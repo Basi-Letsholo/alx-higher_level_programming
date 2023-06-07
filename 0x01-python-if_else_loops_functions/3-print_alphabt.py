2c80efc4a292528d5c47df3ec1ce8c98258408f2
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-06-06 17:52:54 -0700

    update task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(ord('a'), ord('z') + 1) and (i != ord('e')):
-    print(("{}").format(chr(i)))
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print(("{}").format(chr(i)), end='')
