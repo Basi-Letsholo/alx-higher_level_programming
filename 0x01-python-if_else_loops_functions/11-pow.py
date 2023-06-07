50dd10e6af07b427c162daed36818abfcca368d1
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-06-06 17:44:15 -0700

    pycodestlye fix

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -5,14 +5,14 @@ def pow(a, b):
         ans = 1
     elif b > 0:
         ans = 1
-        for i in range (0, b):
+        for i in range(0, b):
             ans = ans * a
     elif b < 0:
         ans = 1
         c = -b
         n = 1
-        for i in range (0, c):
+        for i in range(0, c):
             n = n * a
         ans = 1 / n
-    
+
     return ans
