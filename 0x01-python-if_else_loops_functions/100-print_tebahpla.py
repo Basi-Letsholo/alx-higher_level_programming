ade4b25f390bf131da2215f838cb7a8323b872da
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-06-06 18:39:23 -0700

    advanced task 100

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for i in range(ord('z'), ord('a') - 1, -1):
+    if (i % 2) == 0:
+        x = i
+    elif (i % 2) != 0:
+        x = i - ord('a') + ord('A')
+    print(chr(x), end='')
