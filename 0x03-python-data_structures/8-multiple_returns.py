c43e15b737ffbc4e72369ce7079ba290c506c859
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-06-12 18:14:00 -0700

    fixed return for edge case

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 def multiple_returns(sentence):
     len_sentence = len(sentence)
     if len_sentence == 0:
-        return new_tuple(0, None)
+        return (0, None)
     else:
         new_tuple = (len_sentence, sentence[0])
         return new_tuple
