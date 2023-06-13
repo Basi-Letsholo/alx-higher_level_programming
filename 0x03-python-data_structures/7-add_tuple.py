b9a6d8fb2a095fb858f1cdb1af6a690eba8608cf
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-06-13 10:32:21 -0700

    update 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) <= 1:
-        a_2 = 0
-        if len(tuple_a) == 0:
-            a_1 = 0
-        elif len(tuple_a) == 1:
-            a_1 = tuple_a[0]
-    if len(tuple_b) <= 1:
-        b_2 = 0
-        if len(tuple_b) == 0:
-            b_1 = 1
-        elif len(tuple_b) == 1:
-            b_1 = tuple_b[0]
-    else:
-        a_1 = tuple_a[0]
-        a_2 = tuple_a[1]
-        b_1 = tuple_b[0]
-        b_2 = tuple_b[1]
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
 
-    new_tup = ((a_1 + b_1), (a_2 + b_2))
+    if len_a < 2:
+        if len_a == 1:
+            list_a = [(tuple_a[0]), 0]
+        elif len_a < 1:
+            list_a = [0, 0]
+    elif len_a >= 2:
+        list_a = [(tuple_a[0]), (tuple_a[1])]
+
+    if len_b < 2:
+        if len_b == 1:
+            list_b = [(tuple_b[0]), 0]
+        elif len_b < 1:
+            list_b = [0, 0]
+    elif len_b >= 2:
+        list_b = [(tuple_b[0]), (tuple_b[1])]
+
+    new_tup = ((list_a[0] + list_b[0]), (list_a[1] + list_b[1]))
     return new_tup
