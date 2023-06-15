9544ced9802f48bb5050e7c852d6a4832152d9c3
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-06-14 17:24:24 -0700

    pycodestyle fix

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    
     sorted_dict = {k: v for k, v in sorted(a_dictionary.items())}
 
     for key, value in sorted_dict.items():
