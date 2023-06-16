d243936a7258447972f181e74c41c536a9f3da01
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-06-16 11:59:08 -0700

    edge cases

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if roman_string is None or roman_string is not str:
+    if roman_string is None or type(roman_string) is not str:
         return 0
 
     list_str = list(roman_string)
