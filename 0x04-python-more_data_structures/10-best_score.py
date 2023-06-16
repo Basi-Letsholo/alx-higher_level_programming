d39d2b02ad293409652b9da4ade4ad013d202b0f
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-06-16 07:05:40 -0700

    fix: edge cases

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) < 1:
         return None
 
     score_list = []
--- 0x04-python-more_data_structures/10-main.py
+++ 0x04-python-more_data_structures/10-main.py
@@ -7,3 +7,7 @@ print("Best score: {}".format(best_key))
 
 best_key = best_score(None)
 print("Best score: {}".format(best_key))
+
+a_dictionary = {}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
