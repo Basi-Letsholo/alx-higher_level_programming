adc309b73070a2329bcf96834753d3485d276b43
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-06-26 14:58:38 -0700

    tasks 4 - 6

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+
+    for i in range(0, list_length):
+        try:
+            new_list.append(my_list_1[i] / my_list_2[i])
+
+        except:
+
+        finally:
+
+    return new_list
