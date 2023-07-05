89fc0ec84055ad00a92989b02d14259a3ed9cebb
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-07-05 05:30:51 +0200

    testing text indent

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,6 +9,9 @@ def text_indentation(text):
 
     if type(text) is not str:
         raise TypeError("text must be a string")
+
+    if len(text) == 0:
+        return
     
     new_text = ''
     i = 0
