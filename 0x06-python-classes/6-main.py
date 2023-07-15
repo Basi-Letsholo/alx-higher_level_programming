4004d1c58f0165cc8275f3e0d2562ee63e346e75
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-07-15 08:08:00 -0700

    update 6

--- 0x06-python-classes/6-main.py
+++ 0x06-python-classes/6-main.py
@@ -16,7 +16,8 @@ my_square_3.my_print()
 
 print("--")
 
-my_square_4 = Square(3, "position")
-my_square_4.my_print()
-
+try:
+    my_square = Square(3, "Position")
+except Exception as e:
+    print(e)
 print("--")
