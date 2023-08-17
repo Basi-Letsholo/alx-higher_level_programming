77c9ec1ba57ac86e0442d966f1f9ee5daeb0a74b
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-08-17 10:26:22 -0700

    join tables

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,2 @@
+-- Lists all cities contained in the datbase hbtn_0d_usa
+SELECT  cities.id, cities.name, states.name AS state_name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
