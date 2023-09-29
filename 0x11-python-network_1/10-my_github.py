c05916584a11602a67b887354acabec34f5007a4
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-09-29 15:59:37 -0700

    github api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""github api."""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = f'https://api.github.com/user'
+
+    headers = {'Authorization': f'Basi {username}:{password}'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data.get('id')
+        if user_id is not None:
+            print(user_id)
+        else:
+            print(None)
+    else:
+        print(None)
