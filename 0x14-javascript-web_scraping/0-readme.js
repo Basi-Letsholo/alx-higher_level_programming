e054f3fa1d645e897eb3bb04c974a7873f24937c
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-10-24 09:50:33 -0700

    semistandard fixes

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(1)
-const fs = require('fs')
+const args = process.argv.slice(1);
+const fs = require('fs');
 
-const file_path = args[1]
+const filePath = args[1];
 
-fs.readFile(file_path, 'utf8', (err, data) => {
-	if (err) throw err;
-	console.log(data.toString());
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
 });
