297ec0972a60891581999a00f17ca226797ba9c0
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-10-25 07:32:04 -0700

    characters

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(1);
+const request = require('request');
+
+const movieId = parseInt(args[1], 10);
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+request(url, (err, response, body) => {
+  if (err) console.error(err);
+
+	const movieData = JSON.parse(body);
+	const charUrls = movieData.characters;
+
+	for (const character of charUrls) {
+		request(character, (err, response, body) => {
+			if (err) console.log(err);
+
+			let charData = JSON.parse(body);
+			console.log(charData.name);
+		});
+	};
+});
