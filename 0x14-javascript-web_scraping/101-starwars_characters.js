cb4dc542d42c762225ca4920b6189a8e2f16e546
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-10-25 10:06:56 -0700

    chars in order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,22 +2,44 @@
 
 const args = process.argv.slice(1);
 const request = require('request');
+const util = require('util');
+const promisifyRequest = util.promisify(request);
 
 const movieId = parseInt(args[1], 10);
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-request(url, (err, response, body) => {
-  if (err) console.error(err);
-
-  const movieData = JSON.parse(body);
-  const charUrls = movieData.characters;
-
-  for (const character of charUrls) {
-    request(character, (err, response, body) => {
-      if (err) console.log(err);
-
-      const charData = JSON.parse(body);
-      console.log(charData.name);
-    });
+const fetchCharacter = async (charUrl) => {
+  try {
+    const charData = await promisifyRequest(charUrl);
+    const parsedCharData = JSON.parse(charData.body);
+    console.log(parsedCharData.name);
+  } catch (err) {
+    console.error(err);
   }
-});
+};
+
+(async () => {
+  try {
+    const movieData = await promisifyRequest(url);
+    const parsedMovieData = JSON.parse(movieData.body);
+    const charUrls = parsedMovieData.characters;
+
+    const idList = [];
+    for (const charId of charUrls) {
+      const match = charId.match(/\d+/);
+      if (match) {
+        const foundId = parseInt(match[0], 10);
+        idList.push(foundId);
+      }
+    }
+
+    idList.sort((a, b) => a - b);
+
+    for (const num of idList) {
+      const charUrl = 'https://swapi-api.alx-tools.com/api/people/' + num;
+      await fetchCharacter(charUrl);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+})();
